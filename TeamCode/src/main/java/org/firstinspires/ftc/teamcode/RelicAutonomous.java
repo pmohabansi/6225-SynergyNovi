7491b7905fb31cdc44434030a531dbe4e2d8b7ae
Piyush Mohabansi <piyush.mohabansi@gmail.com>
2017-10-10 20:11:32 -0400

    Tried to make more dynamic. We need to test though.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicAutonomous.java
@@ -1,30 +1,6 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Developer: Apar Mohabansi
+ * Date: 09/27/2017
  */
 package org.firstinspires.ftc.teamcode;
 
@@ -46,15 +22,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.VuMarkInstanceId;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 
-
-@Autonomous(name="Concept: RelicAutonomous", group ="Concept")
+@Autonomous(name = "Concept: RelicAutonomous", group = "Concept")
 //@Disabled
 public class RelicAutonomous extends LinearOpMode {
 
@@ -69,22 +43,21 @@ public class RelicAutonomous extends LinearOpMode {
     VuforiaTrackable relicTemplate;
 
     boolean isCryptoKeyFound = false;
-    String  CryptoKeyFoundPosition = "";
+    String CryptoKeyFoundPosition = "";
 
-    @Override public void runOpMode() {
+    @Override
+    public void runOpMode() {
         InitializeColorSensorStuff();
         InitializeIdentifyCryptoKeyStuffs();
 
         waitForStart();
 
-        relicTrackables.activate();
-
         while (opModeIsActive()) {
 
             try {
                 IdentifyColor(); // actually execute the sample
-            } catch (Exception e){}
-            finally {
+            } catch (Exception e) {
+            } finally {
                 // On the way out, *guarantee* that the background is reasonable. It doesn't actually start off
                 // as pure white, but it's too much work to dig out what actually was used, and this is good
                 // enough to at least make the screen reasonable again.
@@ -94,10 +67,10 @@ public class RelicAutonomous extends LinearOpMode {
                         relativeLayout.setBackgroundColor(Color.WHITE);
                     }
                 });
+            }
 
-                if( !isCryptoKeyFound ) {
-                    IdentifyCryptoKeyFunctionality();
-                }
+            if (!isCryptoKeyFound) {
+                IdentifyCryptoKeyFunctionality();
             }
         }
     }
@@ -122,7 +95,9 @@ public class RelicAutonomous extends LinearOpMode {
         relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
     }
 
-    protected void IdentifyCryptoKeyFunctionality(){
+    protected void IdentifyCryptoKeyFunctionality() {
+
+        relicTrackables.activate();
 
         /**
          * See if any of the instances of {@link relicTemplate} are currently visible.
@@ -141,11 +116,11 @@ public class RelicAutonomous extends LinearOpMode {
                 /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
                  * it is perhaps unlikely that you will actually need to act on this pose information, but
                  * we illustrate it nevertheless, for completeness. */
-            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) relicTemplate.getListener()).getPose();
             telemetry.addData("Pose", format(pose));
 
             CryptoKeyFoundPosition = "vuMark:" + vuMark.toString();
-            if(vuMark.toString().toUpperCase().contains("LEFT") ||
+            if (vuMark.toString().toUpperCase().contains("LEFT") ||
                     vuMark.toString().toUpperCase().contains("CENTER") ||
                     vuMark.toString().toUpperCase().contains("RIGHT")) {
                 isCryptoKeyFound = true;
@@ -167,15 +142,15 @@ public class RelicAutonomous extends LinearOpMode {
                 double rY = rot.secondAngle;
                 double rZ = rot.thirdAngle;
             }
-        }
-        else {
+        } else {
             telemetry.addData("VuMark", "not visible");
         }
 
+        relicTrackables.deactivate();
         telemetry.update();
     }
 
-    protected void InitializeColorSensorStuff(){
+    protected void InitializeColorSensorStuff() {
         int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
         relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
     }
@@ -192,89 +167,89 @@ public class RelicAutonomous extends LinearOpMode {
         // If possible, turn the light on in the beginning (it might already be on anyway,
         // we just make sure it is if we can).
         if (colorSensor instanceof SwitchableLight) {
-            ((SwitchableLight)colorSensor).enableLight(true);
+            ((SwitchableLight) colorSensor).enableLight(true);
         }
 
         // Loop until we are asked to stop
-            // Check the status of the x button on the gamepad
-            bCurrState = gamepad1.x;
+        // Check the status of the x button on the gamepad
+        bCurrState = gamepad1.x;
 
-            // If the button state is different than what it was, then act
-            if (bCurrState != bPrevState) {
-                // If the button is (now) down, then toggle the light
-                if (bCurrState) {
-                    if (colorSensor instanceof SwitchableLight) {
-                        SwitchableLight light = (SwitchableLight)colorSensor;
-                        light.enableLight(!light.isLightOn());
-                    }
+        // If the button state is different than what it was, then act
+        if (bCurrState != bPrevState) {
+            // If the button is (now) down, then toggle the light
+            if (bCurrState) {
+                if (colorSensor instanceof SwitchableLight) {
+                    SwitchableLight light = (SwitchableLight) colorSensor;
+                    light.enableLight(!light.isLightOn());
                 }
             }
-            bPrevState = bCurrState;
+        }
+        bPrevState = bCurrState;
 
-            // Read the sensor
-            NormalizedRGBA colors = colorSensor.getNormalizedColors();
+        // Read the sensor
+        NormalizedRGBA colors = colorSensor.getNormalizedColors();
 
-            /** Use telemetry to display feedback on the driver station. We show the conversion
-             * of the colors to hue, saturation and value, and display the the normalized values
-             * as returned from the sensor.
-             * @see <a href="http://infohost.nmt.edu/tcc/help/pubs/colortheory/web/hsv.html">HSV</a>*/
+        /** Use telemetry to display feedback on the driver station. We show the conversion
+         * of the colors to hue, saturation and value, and display the the normalized values
+         * as returned from the sensor.
+         * @see <a href="http://infohost.nmt.edu/tcc/help/pubs/colortheory/web/hsv.html">HSV</a>*/
 
-            Color.colorToHSV(colors.toColor(), hsvValues);
-            telemetry.addLine()
-                    .addData("H", "%.3f", hsvValues[0])
-                    .addData("S", "%.3f", hsvValues[1])
-                    .addData("V", "%.3f", hsvValues[2]);
-            telemetry.addLine()
-                    .addData("a", "%.3f", colors.alpha)
-                    .addData("r", "%.3f", colors.red)
-                    .addData("g", "%.3f", colors.green)
-                    .addData("b", "%.3f", colors.blue);
+        Color.colorToHSV(colors.toColor(), hsvValues);
+        telemetry.addLine()
+                .addData("H", "%.3f", hsvValues[0])
+                .addData("S", "%.3f", hsvValues[1])
+                .addData("V", "%.3f", hsvValues[2]);
+        telemetry.addLine()
+                .addData("a", "%.3f", colors.alpha)
+                .addData("r", "%.3f", colors.red)
+                .addData("g", "%.3f", colors.green)
+                .addData("b", "%.3f", colors.blue);
 
-            /** We also display a conversion of the colors to an equivalent Android color integer.
-             * @see Color */
-            int color = colors.toColor();
-            telemetry.addLine("raw Android color: ")
-                    .addData("a", "%02x", Color.alpha(color))
-                    .addData("r", "%02x", Color.red(color))
-                    .addData("g", "%02x", Color.green(color))
-                    .addData("b", "%02x", Color.blue(color));
-
-            // Balance the colors. The values returned by getColors() are normalized relative to the
-            // maximum possible values that the sensor can measure. For example, a sensor might in a
-            // particular configuration be able to internally measure color intensity in a range of
-            // [0, 10240]. In such a case, the values returned by getColors() will be divided by 10240
-            // so as to return a value it the range [0,1]. However, and this is the point, even so, the
-            // values we see here may not get close to 1.0 in, e.g., low light conditions where the
-            // sensor measurements don't approach their maximum limit. In such situations, the *relative*
-            // intensities of the colors are likely what is most interesting. Here, for example, we boost
-            // the signal on the colors while maintaining their relative balance so as to give more
-            // vibrant visual feedback on the robot controller visual display.
-            float max = Math.max(Math.max(Math.max(colors.red, colors.green), colors.blue), colors.alpha);
-            colors.red   /= max;
-            colors.green /= max;
-            colors.blue  /= max;
-            color = colors.toColor();
-
-            telemetry.addLine(CryptoKeyFoundPosition);
-            telemetry.addLine("normalized color:  ")
+        /** We also display a conversion of the colors to an equivalent Android color integer.
+         * @see Color */
+        int color = colors.toColor();
+        telemetry.addLine("raw Android color: ")
                 .addData("a", "%02x", Color.alpha(color))
                 .addData("r", "%02x", Color.red(color))
                 .addData("g", "%02x", Color.green(color))
                 .addData("b", "%02x", Color.blue(color));
 
-            telemetry.update();
+        // Balance the colors. The values returned by getColors() are normalized relative to the
+        // maximum possible values that the sensor can measure. For example, a sensor might in a
+        // particular configuration be able to internally measure color intensity in a range of
+        // [0, 10240]. In such a case, the values returned by getColors() will be divided by 10240
+        // so as to return a value it the range [0,1]. However, and this is the point, even so, the
+        // values we see here may not get close to 1.0 in, e.g., low light conditions where the
+        // sensor measurements don't approach their maximum limit. In such situations, the *relative*
+        // intensities of the colors are likely what is most interesting. Here, for example, we boost
+        // the signal on the colors while maintaining their relative balance so as to give more
+        // vibrant visual feedback on the robot controller visual display.
+        float max = Math.max(Math.max(Math.max(colors.red, colors.green), colors.blue), colors.alpha);
+        colors.red /= max;
+        colors.green /= max;
+        colors.blue /= max;
+        color = colors.toColor();
 
-            // convert the RGB values to HSV values.
-            Color.RGBToHSV(Color.red(color), Color.green(color), Color.blue(color), hsvValues);
+        telemetry.addLine(CryptoKeyFoundPosition);
+        telemetry.addLine("normalized color:  ")
+                .addData("a", "%02x", Color.alpha(color))
+                .addData("r", "%02x", Color.red(color))
+                .addData("g", "%02x", Color.green(color))
+                .addData("b", "%02x", Color.blue(color));
 
-            // change the background color to match the color detected by the RGB sensor.
-            // pass a reference to the hue, saturation, and value array as an argument
-            // to the HSVToColor method.
-            relativeLayout.post(new Runnable() {
-                public void run() {
-                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-                }
-            });
+        telemetry.update();
+
+        // convert the RGB values to HSV values.
+        Color.RGBToHSV(Color.red(color), Color.green(color), Color.blue(color), hsvValues);
+
+        // change the background color to match the color detected by the RGB sensor.
+        // pass a reference to the hue, saturation, and value array as an argument
+        // to the HSVToColor method.
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+            }
+        });
     }
 }
 
